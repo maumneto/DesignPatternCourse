f382fab0bd11846df91a3c527fe70979c3c4714b
Mauricio Neto <maumneto@gmail.com>
2021-06-12 08:19:31 -0300

    feat: add new code

---
+++ SOLID/SRP/BadSRPCode.py
@@ -0,0 +1,16 @@
+class GeometricFormat:
+    def __init__(self, sideLength = 0, height = 0):
+        self.sideLength = sideLength
+        self.height = height
+    
+    def area_square(self):
+        return sideLength * sideLength
+
+    def perimeter_squre(self):
+        return sideLength * 4
+
+    def area_triangle(self):
+        return 
+
+    def draw_square(self):
+        printf("Drawing a Square!")
